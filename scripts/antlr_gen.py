eeefe4b5f2230d17f5c8a8fe1f94102494f84a6e
Perfectrum <38108124+Perfectrum@users.noreply.github.com>
2023-05-16 00:19:54 +0300

    Update antlr_gen.py

--- scripts/antlr_gen.py
+++ scripts/antlr_gen.py
@@ -5,7 +5,7 @@ import shared
 def main():
     shared.configure_python_path()
     subprocess.check_call(
-        "antlr4 project/lang/lang.g4 -Dlanguage=Python3 -o project/lang/antlrgen -Xexact-output-dir -visitor".split(
+        "antlr4 project/lang/lang.g4 -Dlanguage=Python3 -o project/lang/antlrgen -Xexact-output-dir".split(
             " "
         )
     )
